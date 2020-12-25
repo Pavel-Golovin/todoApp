90cd7e1ecee23e829acae0acd748000e4890f0bd
Pavel <Golovin.Pavel@outlook.com>
2020-12-25 05:10:00 +0300

    Перенёс данные в состояние родительского компонента

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,29 +1,32 @@
-import React from "react";
+import React, {Component} from "react";
 import NewTaskForm from "../new-task-form";
 import TaskList from "../task-list";
 import Footer from "../footer";
 import "./app.css";
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    {  className: "completed", label: "Completed task", creationTime: "created 17 seconds ago", id: 1 },
-    {  className: "editing", label: "Editing task", creationTime: "created 5 minutes ago", id: 2 },
-    {  className: null, label: "Active task", creationTime: "created 5 minutes ago", id: 3 }
-  ];
+    state = {
+      todoData: [
+        {className: "completed", label: "Completed task", creationTime: "created 17 seconds ago", id: 1},
+        {className: "editing", label: "Editing task", creationTime: "created 5 minutes ago", id: 2},
+        {className: null, label: "Active task", creationTime: "created 5 minutes ago", id: 3}
+      ],
+    };
 
-  return (
-    <section className="todoapp">
-      <header className="header">
-        <h1>todos</h1>
-        <NewTaskForm />
-      </header>
-      <section className="main">
-        <TaskList todos={todoData}/>
-        <Footer />
-      </section>
-    </section>
-  );
-};
+    render() {
+        return (
+            <section className="todoapp">
+                <header className="header">
+                    <h1>todos</h1>
+                    <NewTaskForm/>
+                </header>
+                <section className="main">
+                    <TaskList todos={this.state.todoData}/>
+                    <Footer/>
+                </section>
+            </section>
+        );
+    }
+}
 
-export default App;
