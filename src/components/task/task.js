eba8edac0aaf859a28277d651d8ecc05659cfb43
Pavel <Golovin.Pavel@outlook.com>
2020-12-25 09:15:29 +0300

    Task тоже теперь компонент класса

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, {Component} from "react";
 import "./task.css";
 
-const Task = (props) => {
-  return (
-    <div className="view">
-      <input className="toggle" type="checkbox"/>
-      <label>
-        <span className="description">{props.label}</span>
-        <span className="created">{props.creationTime}</span>
-      </label>
-      <button className="icon icon-edit"></button>
-      <button className="icon icon-destroy"></button>
-    </div>
-  );
-};
+export default class Task extends Component {
 
-export default Task;
+  render() {
+    return (
+      <div className="view">
+        <input className="toggle" type="checkbox"/>
+        <label>
+          <span className="description">{this.props.label}</span>
+          <span className="created">{this.props.creationTime}</span>
+        </label>
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy"></button>
+      </div>
+    );
+  }
+};
