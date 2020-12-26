a93a728e924e752de959039b0a9d2e100793180b
Pavel <Golovin.Pavel@outlook.com>
2020-12-26 19:04:24 +0300

    Рефакторинг

--- src/components/tasks-filter/tasks-filter.js
+++ src/components/tasks-filter/tasks-filter.js
@@ -5,19 +5,17 @@ export default class TasksFilter extends Component {
 
   render() {
     return (
-      <form>
-        <ul className="filters">
-          <li>
-            <button className="selected">All</button>
-          </li>
-          <li>
-            <button>Active</button>
-          </li>
-          <li>
-            <button>Completed</button>
-          </li>
-        </ul>
-      </form>
+      <ul className="filters">
+        <li>
+          <button className="selected">All</button>
+        </li>
+        <li>
+          <button>Active</button>
+        </li>
+        <li>
+          <button>Completed</button>
+        </li>
+      </ul>
     )
   };
 };
