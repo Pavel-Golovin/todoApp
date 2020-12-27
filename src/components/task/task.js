afdbe1536aca8b5c186fd2fe025c9ae450e0a2bc
Pavel <Golovin.Pavel@outlook.com>
2020-12-27 09:42:31 +0300

    Реализовал обновление "created N seconds / minutes ago"

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -5,8 +5,26 @@ import "./task.css";
 export default class Task extends Component {
 
   state = {
-    value: this.props.text
+    value: this.props.text,
+    distance: formatDistanceToNow(this.props.creationTime, {includeSeconds: true})
   }
+
+  update = () => {
+    this.setState({
+      distance: formatDistanceToNow(this.props.creationTime, {includeSeconds: true})
+    });
+  };
+
+  componentDidMount = () => {
+    this.timerID = setInterval(
+      () => this.update(),
+      1000
+    );
+  };
+
+  componentWillUnmount = () => {
+    clearTimeout(this.timerID);
+  };
 
   onFormSubmit = (e) => {
     e.preventDefault();
@@ -24,7 +42,7 @@ export default class Task extends Component {
 
   render() {
 
-    const {text, className, creationTime, onCompleted, onDestroyed, onEdit} = this.props;
+    const {text, className, onCompleted, onDestroyed, onEdit} = this.props;
 
     return (
       <form onSubmit={this.onFormSubmit}>
@@ -34,7 +52,9 @@ export default class Task extends Component {
             onClick={onCompleted}
           >
             <span className="description">{text}</span>
-            <span className="created">{`created ${formatDistanceToNow(creationTime, {includeSeconds: true})} ago`}</span>
+            <span className="created">{
+              `created ${this.state.distance} ago`
+            }</span>
           </label>
             <button type="button" className="icon icon-edit"
               onClick={(e) => {
