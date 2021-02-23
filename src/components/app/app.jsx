1a6284d4966d6e0482143e266cca8e2112751933
Pavel <Golovin.Pavel@outlook.com>
2021-02-23 11:48:56 +0300

    Переписал App в синтаксисе хуков

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,93 +1,82 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import NewTaskForm from '../new-task-form/index';
 import TaskList from '../task-list/index';
 import Footer from '../footer/index';
 import './app.css';
 
-export default class App extends Component {
-  maxId = 9;
+const App = () => {
+  const [tasks, updateTasks] = useState([]);
+  const [filterName, changeFilterName] = useState('All');
+  const [maxId, updateMaxId] = useState(9);
 
-  state = {
-    tasks: [],
-    filterName: 'All',
-  };
+  const getTaskIndex = (id) => tasks.findIndex((el) => el.id === id);
 
-  getTaskIndex = (id) => {
-    const { tasks } = this.state;
-    return tasks.findIndex((el) => el.id === id);
-  };
-
-  getTaskToBeChanged = (id) => {
-    const idx = this.getTaskIndex(id);
-    const { tasks } = this.state;
+  const getTaskToBeChanged = (id) => {
+    const idx = getTaskIndex(id);
     return tasks[idx];
   };
 
-  createTask = (text, min, sec) => ({
-    text,
-    min,
-    sec,
-    creationTime: new Date(),
-    completed: false,
-    toBeEdited: false,
-    id: this.maxId++, // eslint-disable-line no-plusplus
-  });
-
-  changeProp = (id, newTask) => {
-    this.setState(({ tasks }) => {
-      const idx = this.getTaskIndex(id);
-      const newArray = [...tasks.slice(0, idx), newTask, ...tasks.slice(idx + 1)];
-      return { tasks: newArray };
-    });
+  const createTask = (text, min, sec) => {
+    const newTask = {
+      text,
+      min,
+      sec,
+      creationTime: new Date(),
+      completed: false,
+      toBeEdited: false,
+      id: maxId,
+    };
+    updateMaxId((prev) => prev + 1);
+    return newTask;
   };
 
-  addTask = (text, min, sec) => {
+  const changeProp = (id, newTask) => {
+    const idx = getTaskIndex(id);
+    const newArray = [...tasks.slice(0, idx), newTask, ...tasks.slice(idx + 1)];
+    updateTasks(newArray);
+  };
+
+  const addTask = (text, min, sec) => {
     if (!text.trim()) {
       return;
     }
-    this.setState(({ tasks }) => {
-      const newTask = this.createTask(text, min, sec);
-      const newArray = [...tasks.slice(), newTask];
-      return { tasks: newArray };
-    });
+    const newTask = createTask(text, min, sec);
+    const newArray = [...tasks.slice(), newTask];
+    updateTasks(newArray);
   };
 
-  removeTask = (id) => {
-    this.setState(({ tasks }) => {
-      const idx = this.getTaskIndex(id);
-      const newArray = [...tasks.slice(0, idx), ...tasks.slice(idx + 1)];
-      return { tasks: newArray };
-    });
+  const removeTask = (id) => {
+    const idx = getTaskIndex(id);
+    const newArray = [...tasks.slice(0, idx), ...tasks.slice(idx + 1)];
+    updateTasks(newArray);
   };
 
-  changePropCompleted = (id) => {
-    const oldTask = this.getTaskToBeChanged(id);
-    this.changeProp(id, { ...oldTask, completed: !oldTask.completed });
+  const changePropCompleted = (id) => {
+    const oldTask = getTaskToBeChanged(id);
+    changeProp(id, { ...oldTask, completed: !oldTask.completed });
   };
 
-  changePropToBeEdited = (id) => {
-    const oldTask = this.getTaskToBeChanged(id);
-    this.changeProp(id, { ...oldTask, toBeEdited: !oldTask.toBeEdited });
+  const changePropToBeEdited = (id) => {
+    const oldTask = getTaskToBeChanged(id);
+    changeProp(id, { ...oldTask, toBeEdited: !oldTask.toBeEdited });
   };
 
-  changePropText = (id, text) => {
+  const changePropText = (id, text) => {
     if (!text.trim()) {
       return;
     }
-    const oldTask = this.getTaskToBeChanged(id);
-    this.changeProp(id, { ...oldTask, text, toBeEdited: !oldTask.toBeEdited });
+    const oldTask = getTaskToBeChanged(id);
+    changeProp(id, { ...oldTask, text, toBeEdited: !oldTask.toBeEdited });
   };
 
-  filterTasks = (newFilterName) => {
-    const { filterName } = this.state;
+  const filterTasks = (newFilterName) => {
     if (newFilterName === filterName) {
       return;
     }
-    this.setState(() => ({ filterName: newFilterName }));
+    changeFilterName(newFilterName);
   };
 
-  getFilteredTasks = () => {
-    const { tasks, filterName } = this.state;
+  const getFilteredTasks = () => {
     switch (filterName) {
       case 'Completed':
         return tasks.filter((todo) => todo.completed);
@@ -100,42 +89,31 @@ export default class App extends Component {
     }
   };
 
-  delCompletedTasks = () => {
-    this.setState(({ tasks }) => {
-      const newArray = tasks.filter((task) => !task.completed);
-      return {
-        tasks: newArray,
-      };
-    });
+  const delCompletedTasks = () => {
+    const newArray = tasks.filter((task) => !task.completed);
+    updateTasks(newArray);
   };
 
-  getCountActiveTasks = () => {
-    const { tasks } = this.state;
-    return tasks.filter((task) => task.completed === false).length;
-  };
+  const getCountActiveTasks = () => tasks.filter((task) => task.completed === false).length;
 
-  render() {
-    return (
-      <section className="todoapp">
-        <header className="header">
-          <h1>todos</h1>
-          <NewTaskForm onSubmit={(text, min, sec) => this.addTask(text, min, sec)} />
-        </header>
-        <section className="main">
-          <TaskList
-            todos={this.getFilteredTasks()}
-            onDestroyed={this.removeTask}
-            onCompleted={this.changePropCompleted}
-            onEditing={this.changePropToBeEdited}
-            onEditTask={this.changePropText}
-          />
-          <Footer
-            onFilter={this.filterTasks}
-            onClear={this.delCompletedTasks}
-            activeTasksCounter={this.getCountActiveTasks}
-          />
-        </section>
+  return (
+    <section className="todoapp">
+      <header className="header">
+        <h1>todos</h1>
+        <NewTaskForm onSubmit={(text, min, sec) => addTask(text, min, sec)} />
+      </header>
+      <section className="main">
+        <TaskList
+          todos={getFilteredTasks()}
+          onDestroyed={removeTask}
+          onCompleted={changePropCompleted}
+          onEditing={changePropToBeEdited}
+          onEditTask={changePropText}
+        />
+        <Footer onFilter={filterTasks} onClear={delCompletedTasks} activeTasksCounter={getCountActiveTasks} />
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
+
+export default App;
