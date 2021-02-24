df0bcd4fc47a462474307e3c9e5ff84e3bc25cf7
Pavel <Golovin.Pavel@outlook.com>
2021-02-24 11:24:42 +0300

    Отрефакторил компонент Footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,19 +3,15 @@ import PropTypes from 'prop-types';
 import TasksFilter from '../tasks-filter/index';
 import './footer.css';
 
-const Footer = (props) => {
-  const { activeTasksCount, onClear } = props;
-
-  return (
-    <footer className="footer">
-      <span className="todo-count">{activeTasksCount} items left</span>
-      <TasksFilter onFilter={(filterName) => props.onFilter(filterName)} />
-      <button type="button" className="clear-completed" onClick={onClear}>
-        Clear completed
-      </button>
-    </footer>
-  );
-};
+const Footer = ({ activeTasksCount, onClear, onFilter }) => (
+  <footer className="footer">
+    <span className="todo-count">{activeTasksCount} items left</span>
+    <TasksFilter onFilter={(filterName) => onFilter(filterName)} />
+    <button type="button" className="clear-completed" onClick={onClear}>
+      Clear completed
+    </button>
+  </footer>
+);
 
 Footer.defaultProps = {
   activeTasksCount: 0,
